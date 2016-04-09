a00f6a289843f39bf6a631bc815315ee79076455
Fabio R <fabio@ribei.ro>
2016-04-09 10:45:29 -0400

    date test

--- test.js
+++ test.js
@@ -1,9 +1,21 @@
 var http = require('http');
 var d = new Date();
 
+function getTime() {
+
+	var d = new Date();
+  		var dh = d.getHours();
+  		var dm = d.getMinutes();
+  		dm = (dm > 10) ? dm : '0'+dm;
+  		var mtime = dh + ':' + dm;
+
+  		return mtime;
+}
+
+
 http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('testing. . .\n' + d);
+  response.end('testing. . .\n' + getTime());
 }).listen(3001);
 
 console.log('Server running at port 3001');  
